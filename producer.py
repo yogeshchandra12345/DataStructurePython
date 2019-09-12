b85a7eb446e1423a67166d1e1106a68128d59a5a
Yogesh Chandra <yogesh.chandra.iitbhu@gmail.com>
2019-09-12 11:25:48 +0530

    Delete producer.py

--- producer.py
+++
@@ -1,6 +0,0 @@
-from kafka import KafkaProducer
-producer = KafkaProducer(bootstrap_servers='localhost:9092')
-for _ in range(100):
-    print('before')
-    producer.send('test', b'some_message_bytes')
-    print('after')
