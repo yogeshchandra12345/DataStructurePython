0607a2e2ae0d7444e40d5e3abda7cf86eac17237
Yogesh Chandra <yogesh.chandra.iitbhu@gmail.com>
2019-09-16 17:24:07 +0530

    knapsack code added

---
+++ dynamic_programming/knapsack.py
@@ -0,0 +1,19 @@
+def knapsack(weights_, values_, w):
+    n = len(weights_)
+    dp = [[0 for _ in range(0, w+1)] for _ in range(0, n+1)]
+
+    for i in range(1, n+1):
+        for j in range(1, w+1):
+            if weights_[i-1] <= j:
+                dp[i][j] = max(dp[i-1][j], values_[i-1] + dp[i-1][j-weights[i-1]])
+            else:
+                dp[i][j] = dp[i-1][w]
+
+    return dp[n][w]
+
+
+values = [12, 1000, 30, 10, 1000]  # values of each item
+weights = [19, 120, 20, 1, 120]  # weights of each item in same order
+W = 40
+print(knapsack(weights, values, W))
+
